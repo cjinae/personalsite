96284682b39b0c761dd8f875105ba7f17afd3f54
Christine Lee <christine.jinae@gmail.com>
2013-11-09 23:45:29 -0500

    adds type effect

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -68,6 +68,127 @@ jQuery(function($){
 
 	})();
 
+(function ($) {
+  // writes the string
+  //
+  // @param jQuery $target
+  // @param String str
+  // @param Numeric cursor
+  // @param Numeric delay
+  // @param Function cb
+  // @return void
+  function typeString($target, str, cursor, delay, cb) {
+    $target.html(function (_, html) {
+      return html + str[cursor];
+    });
+
+    if (cursor < str.length - 1) {
+      setTimeout(function () {
+        typeString($target, str, cursor + 1, delay, cb);
+      }, delay);
+    }
+    else {
+      cb();
+    }
+  }
+
+  // clears the string
+  //
+  // @param jQuery $target
+  // @param Numeric delay
+  // @param Function cb
+  // @return void
+  function deleteString($target, delay, cb) {
+    var length;
+
+    $target.html(function (_, html) {
+      length = html.length;
+      return html.substr(0, length - 1);
+    });
+
+    if (length > 1) {
+      setTimeout(function () {
+        deleteString($target, delay, cb);
+      }, delay);
+    }
+    else {
+      cb();
+    }
+  }
+
+  // jQuery hook
+  $.fn.extend({
+    teletype: function (opts) {
+      var settings = $.extend({}, $.teletype.defaults, opts);
+
+      return $(this).each(function () {
+        (function loop($tar, idx) {
+          // type
+          typeString($tar, settings.text[idx], 0, settings.delay, function () {
+            // delete
+            setTimeout(function () {
+              deleteString($tar, settings.delay, function () {
+                loop($tar, (idx + 1) % settings.text.length);
+              });
+            }, settings.pause);
+          });
+
+        }($(this), 0));
+      });
+    }
+  });
+
+  // plugin defaults  
+  $.extend({
+    teletype: {
+      defaults: {
+        delay: 100,
+        pause: 5000,
+        text: []
+      }
+    }
+  });
+}(jQuery));
+
+//init
+$('#target').teletype({
+  text: [
+    'planning',
+    'user stories',
+    'training',
+    'adoption'
+   ]
+});
+
+$('#cursor').teletype({
+  text: ['_', ' '],
+  delay: 0,
+  pause: 500
+});
+
+$('#target2').teletype({
+  text: [
+    'alternative solutions',
+    'thinking ouside the box'
+   ]
+});
+
+$('#target3').teletype({
+  text: [
+    'analytics driven ',
+    'budgeting',
+    'metrics',
+   ]
+});
+
+$('#target4').teletype({
+  text: [
+    'html',
+    'css',
+    'javascript',
+    'ruby on rails'
+   ]
+});
 
 //global end
 });
